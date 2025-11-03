43550fdfbd5af548a142a95b2e15df70ee142ff2
reham.essa <rehamessa96@gmail.com>
2025-11-03 08:58:10 +0200

    Handle Consistency

--- Silver Layer Scripts/Transformation/Transformation Customer AZ.sql
+++ Silver Layer Scripts/Transformation/Transformation Customer AZ.sql
@@ -1,10 +1,16 @@
 --Handle BDATE Out Of Range Dates
+-- Handle Consistency
 SELECT 
        CASE WHEN CID LIKE 'NAS%' THEN SUBSTRING(CID,4,LEN(CID))
        ELSE CID
        END AS CID
+
       ,CASE WHEN BDATE>GETDATE() THEN NULL 
       ELSE BDATE
       END AS BDATE
-      ,GEN
+
+      ,CASE WHEN UPPER(TRIM(GEN)) IN('F','FEMALE') THEN 'Female'
+            WHEN UPPER(TRIM(GEN)) IN('M','MALE') THEN 'Male'
+        ELSE 'N/A'
+        END AS GEN
   FROM dwh_sales.bronze.ERP_CUST_AZ12 
