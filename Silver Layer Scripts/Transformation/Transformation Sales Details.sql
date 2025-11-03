55cec58967015a3583d1630249fc019781fb7203
reham.essa <rehamessa96@gmail.com>
2025-11-03 02:31:07 +0200

    Silver Layer Load Data after Transformation

--- Silver Layer Scripts/Transformation/Transformation Sales Details.sql
+++ Silver Layer Scripts/Transformation/Transformation Sales Details.sql
@@ -1,3 +1,35 @@
+-- MODIFR THE DATES TYPE
+IF OBJECT_ID ('silver.crm_sales_details','U') IS NOT NULL
+DROP TABLE silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details(
+sls_ord_num	 NVARCHAR(50),
+sls_prd_key  NVARCHAR(50),
+sls_cust_id INT,
+sls_order_dt DATE,
+sls_ship_dt DATE,
+sls_due_dt DATE,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT,
+dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+------------------------------------------
+---** Load data 
+
+INSERT INTO silver.crm_sales_details(
+sls_ord_num	 ,
+sls_prd_key  ,
+sls_cust_id,
+sls_order_dt,
+sls_ship_dt,
+sls_due_dt ,
+sls_sales ,
+sls_quantity ,
+sls_price
+)
+
+
 -- Handle Null Date and transform it
 -- Handle Sales Number <0 OR NULLS
 ---Handle Price Number <0 OR NULLS
@@ -11,7 +43,8 @@ SELECT
 
       ,CASE WHEN  sls_ship_dt = 0 OR LEN(sls_ship_dt) !=8 THEN NULL
       ELSE CAST(CAST ( sls_ship_dt AS VARCHAR )AS DATE )
-      END AS  sls_ship_dt
+      END AS  
+      sls_ship_dt
     
       ,CASE WHEN   sls_due_dt = 0 OR LEN(sls_due_dt) !=8 THEN NULL
       ELSE CAST(CAST (sls_due_dt AS VARCHAR )AS DATE )
