dab10cb902df72724a58e0fd016b80cd2e1464c4
reham.essa <rehamessa96@gmail.com>
2025-11-04 13:46:32 +0200

    Gold layer|git only the last product

--- Gold Layer Scripts/Build Product Dimension.sql
+++ Gold Layer Scripts/Build Product Dimension.sql
@@ -1,4 +1,4 @@
--- 
+-- git only the current product
 SELECT pn.prd_id
       ,pn.cat_id
       ,pn.prd_key
@@ -7,6 +7,13 @@ SELECT pn.prd_id
       ,pn.prd_line
       ,pn.prd_start_dt
       ,pn.prd_end_dt
+      ,pc.CAT
+      ,pc.MAINTENANCE
+      ,pc.SUBCAT
       
   FROM Silver.crm_prd_info AS pn
+  LEFT JOIN
+  SILVER.ERP_PX_CAT_G1V2 AS PC
+  ON pn.cat_id=PC.ID
+  WHERE prd_end_dt IS NULL -- FILTER OUT HISTORICAL DATA
 
