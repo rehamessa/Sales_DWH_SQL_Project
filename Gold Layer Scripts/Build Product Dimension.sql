cb77525f4e695a1acd894f1827ff70373e1e8369
reham.essa <rehamessa96@gmail.com>
2025-11-04 13:40:33 +0200

    Gold layer|Build Product Dimension

---
+++ Gold Layer Scripts/Build Product Dimension.sql
@@ -0,0 +1,12 @@
+-- 
+SELECT pn.prd_id
+      ,pn.cat_id
+      ,pn.prd_key
+      ,pn.prd_nm
+      ,pn.prd_cost
+      ,pn.prd_line
+      ,pn.prd_start_dt
+      ,pn.prd_end_dt
+      
+  FROM Silver.crm_prd_info AS pn
+
