09fd9090d04a6570c891f01565a0db86411c76e4
reham.essa <rehamessa96@gmail.com>
2025-11-05 06:31:18 +0200

    Gold layer| Build Dimension product

--- Gold Layer Scripts/Build Product Dimension.sql
+++ Gold Layer Scripts/Build Product Dimension.sql
@@ -1,15 +1,19 @@
+--CREATE VIEW
+CREATE VIEW gold.dim_products AS
 -- git only the current product
-SELECT pn.prd_id
-      ,pn.cat_id
-      ,pn.prd_key
-      ,pn.prd_nm
-      ,pn.prd_cost
-      ,pn.prd_line
-      ,pn.prd_start_dt
-      ,pn.prd_end_dt
-      ,pc.CAT
+--Create surrget Key
+SELECT
+       ROW_NUMBER()OVER(ORDER BY pn.prd_start_dt ,pn.prd_id ) AS Product_key
+       ,pn.prd_id AS product_id
+      ,pn.cat_id AS Catgory_id
+      ,pn.prd_key AS product_number
+      ,pn.prd_nm  AS product_name
+      ,pn.prd_cost AS cost
+      ,pn.prd_line AS product_line
+      ,pn.prd_start_dt AS start_date
+      ,pc.CAT AS category
       ,pc.MAINTENANCE
-      ,pc.SUBCAT
+      ,pc.SUBCAT AS subcategory
       
   FROM Silver.crm_prd_info AS pn
   LEFT JOIN
