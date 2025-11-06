f20ecd70b4d943230b4701900dafb7a330e8943e
reham.essa <rehamessa96@gmail.com>
2025-11-06 07:52:20 +0200

    Gold layer| replace original Key with surrrgate keys

--- Gold Layer Scripts/Build Fact Sales.sql
+++ Gold Layer Scripts/Build Fact Sales.sql
@@ -1,7 +1,7 @@
-SELECT sd.sls_ord_num
-      ,sd.sls_prd_key
-      ,sd.sls_cust_id
-      ,sd.sls_order_dt
+SELECT sd.sls_ord_num AS order_Number
+      ,pr.product_key
+      ,cu.customer_key
+      ,sd.sls_order_dt AS order_date
       ,sd.sls_ship_dt
       ,sd.sls_due_dt
       ,sd.sls_sales
@@ -9,3 +9,7 @@ SELECT sd.sls_ord_num
       ,sd.sls_price 
 
   FROM Silver.crm_sales_details AS sd
+  LEFT JOIN gold.dim_products AS pr
+  ON sd.sls_prd_key=pr.product_number
+  LEFT JOIN gold.dim_customer AS cu
+  ON sd.sls_cust_id=cu.customer_id
